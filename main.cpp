edfd72ff5d01ea20c6dc8afff7602e2a5a12ff03
Carlos R. Arias <arias@spu.edu>
2019-10-11 10:30:02 -0700

    output is formatted and it looks pretty

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <iomanip>
 #include <cmath>
 using std::cout;
 using std::cin;
 using std::endl;
 using std::cerr;
+using std::setprecision;
+using std::fixed;
+using std::setw;
 
 int main(int argc, char* argv[]){
 	const double PI = 3.1415926535;
@@ -12,15 +16,18 @@ int main(int argc, char* argv[]){
 	cout << "Welcome Austin!" << endl;
 	cout << "Please input the Initial Speed (m/s): ";
 	double speed;
-	while (1){
+	bool correct = false;
+	while (!correct){
 		cin >> speed;
 		if (speed > 0)
-			break;
-		cerr << "Austin! Input only positive numbers" << endl;
+			correct = true;
+		else
+			cerr << "Austin! Input only positive numbers" << endl;
 	}
 	cout << "Please input the Angle (Degrees): ";
 	double degrees;
-	while (1){
+	correct = false;
+	while (!correct){
 		cin >> degrees;
 		if (degrees < 0){
 			cerr << "Austin! You may not use negative degrees" << endl;
@@ -29,16 +36,19 @@ int main(int argc, char* argv[]){
 			cerr << "Austin! Are you crazy? The degree has to be less than 90" << endl;
 			continue;
 		}else{
-			break;
+			correct = true;
 		}	
 	}
 	double radians = PI * degrees / 180; 
 	double distance = speed * speed * sin(2 * radians) / G;
 
 	cout << "Thank you Austin for your input.... " << endl;
-	cout << "The shot with initial speed " << speed 
-	     << " with an angle of " << degrees 
-	     << " will go to " << distance << " meters" << endl;
+	cout << "The shot with initial values: " << endl;
+	cout << "Speed: " << setw(10) << setprecision(2) << fixed << speed << endl;
+	cout << "Degree:"<< setw(10) << setprecision(2) << fixed << degrees << " degrees" << endl;
+	cout << "Degree:"<< setw(10) << setprecision(2) << fixed << radians << " radians" << endl;
+	cout << endl;
+	cout << " will go to " << setprecision(10) << fixed <<  distance << " meters" << endl;
 	cout << "Good bye" << endl << endl;
 	return 0;
 }
